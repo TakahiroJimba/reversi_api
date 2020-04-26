09182b389902ee23525cb1a467b7fb987c5bb494
神馬隆博 <takahiro412.c@gmail.com>
2020-04-27 05:46:18 +0900

    共通処理をゲームコレクションマネージャプログラムとして切り分けたため、不要になったものを削除

--- app/config/const.php
+++ app/config/const.php
@@ -18,13 +18,6 @@
     define('GAME_MODE_ID_ONLINE_ROOM',       21);
     define('GAME_MODE_ID_ONLINE_FREE',       22);
 
-    // // ユーザ
-    // define('USER_NAME_MAX_LENGTH',                  10);
-    // define('USER_PASSWORD_MIN_LENGTH',              8);
-    // define('USER_PASSWORD_MAX_LENGTH',              20);
-    // define('USER_REGISTRATION_PASS_PHRASE_LENGTH',  4);
-    // define('USER_REGISTRATION_AUTH_EXPIRATION',     1);
-
     // 別ドメインからのアクセスを受け付ける場合に必要
     // header("Access-Control-Allow-Origin: *");
 ?>
